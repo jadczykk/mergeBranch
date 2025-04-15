bc27d8ea7cd35cd7ca16df2ff75ee24f060c78bc
jadczykk <karol.jadczyk11@gmail.com>
2025-04-15 23:37:32 +0200

    ignore

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,18 @@
-function show_date() {
-    echo "Dzisiejsza data: $(date +"%Y-%m-%d")"
-}
+#!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
-    show_date
+function generate_logs() {
+    for i in $(seq 1 100); do
+        file_name="log${i}.txt"
+        echo "Nazwa pliku: $file_name" > "$file_name"
+        echo "Nazwa skryptu: $0" >> "$file_name"
+        echo "Data utworzenia: $(date +"%Y-%m-%d %H:%M:%S")" >> "$file_name"
+    done
+    echo "Utworzono 100 plików logx.txt!"
+}
+if [[ "$1" == "--logs" ]]; then
+    generate_logs
 else
-    echo
-    echo
-    echo
+    echo "Użycie: $0 --logs"
+    echo "Flaga:"
+    echo "  --logs   Tworzy 100 plików logx.txt z odpowiednimi informacjami"
 fi
